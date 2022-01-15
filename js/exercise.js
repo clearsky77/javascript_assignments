5968300068b53cc2180379f5f2e26572dc4d8de3
clearsky77 <clearsky3264@gmail.com>
2022-01-15 10:36:58 +0900

    added exercise book

---
+++ js/exercise.js
@@ -0,0 +1,7 @@
+const h2 = document.querySelector("h2");
+const allH2 = document.querySelectorAll(".hello h2");
+const allH2_tag = document.getElementsByTagName("h2");
+
+console.log(h2);
+console.log(allH2);
+console.log(allH2_tag);
